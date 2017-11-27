b4604afbfe3e1eb530d1f82e82ccf7cf885da6f0
stKhaDgar <33667544+stKhaDgar@users.noreply.github.com>
2017-11-27 22:47:16 +0200

    Added arrow's minute + hour

--- Cuckoo-clock/Form1.Designer.cs
+++ Cuckoo-clock/Form1.Designer.cs
@@ -31,6 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
@@ -44,10 +47,30 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(110, 150);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(361, 260);
             this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(188, 536);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(211, 44);
+            this.label3.TabIndex = 3;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -55,6 +78,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(584, 661);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
             this.MaximumSize = new System.Drawing.Size(600, 700);
@@ -63,6 +87,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +96,8 @@
         #endregion
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Cuckoo-clock/Form1.cs
+++ Cuckoo-clock/Form1.cs
@@ -14,19 +14,47 @@ namespace Cuckoo_clock
     public partial class Form1 : Form
     {
         private Graphics gr;
-        private Pen myPen;
-        private float x;
-        private float y;
+        private Pen myPenSeconds;
+        private Pen myPenMinute;
+        private Pen myPenHour;
+        private float xSec;
+        private float ySec;
+        private float xMin;
+        private float yMin;
+        private float xHour;
+        private float yHour;
         int secondNow = DateTime.Now.Second;
+        int minuteNow = DateTime.Now.Minute;
+        int hourNow = DateTime.Now.Hour;
         double angleSeconds;
+        double angleSecondsStatic;
+        double angleMinute;
+        double angleHour;
         Timer secondNows = new Timer();
 
         public Form1()
         {
             InitializeComponent();
             gr = panel1.CreateGraphics();
-            angleSeconds = Math.PI * 1.5;
-            myPen = new Pen(Brushes.Black, 3);
+
+            if(hourNow > 12)
+            {
+                hourNow = hourNow - 12;
+            }
+
+            double angleNow = 0.104 * secondNow;
+            angleSeconds = Math.PI * 1.5 + (angleNow + 0.104);
+            angleSecondsStatic = angleSeconds;
+
+            double angleMinNow = 0.104 * minuteNow;
+            angleMinute = Math.PI * 1.5 + angleMinNow;
+
+            double angleHourNow = 0.52 * hourNow;
+            angleHour = Math.PI * 1.5 + angleHourNow;
+
+            myPenSeconds = new Pen(Brushes.Black, 1);
+            myPenMinute = new Pen(Brushes.Black, 2);
+            myPenHour = new Pen(Brushes.Black, 4);
 
             secondNows.Interval = 1000;
             secondNows.Tick += new EventHandler(this.secondNowTimer);
@@ -37,30 +65,38 @@ namespace Cuckoo_clock
         public void secondNowTimer(object sender, EventArgs e)
         {
             gr.Clear(Color.Gainsboro);
-            
-
-            x = 90 * (float)Math.Cos(angleSeconds) + 180;
-            y = 90 * (float)Math.Sin(angleSeconds) + 130;
-
+            xSec = 120 * (float)Math.Cos(angleSeconds) + 180;
+            ySec = 120 * (float)Math.Sin(angleSeconds) + 130;
             angleSeconds += 0.104;
-            gr.DrawLine(myPen, 180, 130, x, y);
 
-            Console.WriteLine("Рисуй сука");
-        }
+            xMin = 100 * (float)Math.Cos(angleMinute) + 180;
+            yMin = 100 * (float)Math.Sin(angleMinute) + 130;
 
-        private void timeSecondNow(object sender,
-       System.Windows.Forms.PaintEventArgs pe)
-        {
-            Graphics g = pe.Graphics;
-            System.Drawing.Pen myPen;
-            myPen = new System.Drawing.Pen(System.Drawing.Color.Black, 10);
-            
-            int Xposition = (int)(90 * Math.Sin(angleSeconds) + 292);
-            int Yposition = (int)(90 * Math.Cos(angleSeconds) + 281);
-            g.DrawLine(myPen, 292, 281, 400, 400);
-            angleSeconds = angleSeconds + 0.104;
+            xHour = 70 * (float)Math.Cos(angleHour) + 180;
+            yHour = 70 * (float)Math.Sin(angleHour) + 130;
 
-            Console.WriteLine(secondNow);
+            if (angleSeconds > 10.995)
+            {
+                angleSeconds = Math.PI * 1.5;
+                angleMinute += 0.104;
+            }
+
+            if(angleMinute > 10.995)
+            {
+                angleMinute = Math.PI * 1.5;
+                angleHour += 0.52;
+                for(int i=0; i < hourNow; i++)
+                {
+                    label3.Text = label3.Text + "КАР";
+                }
+            }
+
+            gr.DrawLine(myPenSeconds, 180, 130, xSec, ySec);
+            gr.DrawLine(myPenMinute, 180, 130, xMin, yMin);
+            gr.DrawLine(myPenMinute, 180, 130, xHour, yHour);
+
+            Console.WriteLine(angleSeconds);
+            Console.WriteLine(angleMinute);
         }
 
         private void Form1_Load(object sender, EventArgs e)
