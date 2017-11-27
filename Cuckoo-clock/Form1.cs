c27743a25c9daca1f79197dad753f7ca41424395
stKhaDgar <33667544+stKhaDgar@users.noreply.github.com>
2017-11-27 22:51:04 +0200

    Corrected code

--- Cuckoo-clock/Form1.cs
+++ Cuckoo-clock/Form1.cs
@@ -36,30 +36,23 @@ namespace Cuckoo_clock
         {
             InitializeComponent();
             gr = panel1.CreateGraphics();
-
             if(hourNow > 12)
             {
                 hourNow = hourNow - 12;
             }
-
             double angleNow = 0.104 * secondNow;
             angleSeconds = Math.PI * 1.5 + (angleNow + 0.104);
             angleSecondsStatic = angleSeconds;
-
             double angleMinNow = 0.104 * minuteNow;
             angleMinute = Math.PI * 1.5 + angleMinNow;
-
             double angleHourNow = 0.52 * hourNow;
             angleHour = Math.PI * 1.5 + angleHourNow;
-
             myPenSeconds = new Pen(Brushes.Black, 1);
             myPenMinute = new Pen(Brushes.Black, 2);
             myPenHour = new Pen(Brushes.Black, 4);
-
             secondNows.Interval = 1000;
             secondNows.Tick += new EventHandler(this.secondNowTimer);
-            secondNows.Start();
-            
+            secondNows.Start(); 
         }
 
         public void secondNowTimer(object sender, EventArgs e)
@@ -68,10 +61,8 @@ namespace Cuckoo_clock
             xSec = 120 * (float)Math.Cos(angleSeconds) + 180;
             ySec = 120 * (float)Math.Sin(angleSeconds) + 130;
             angleSeconds += 0.104;
-
             xMin = 100 * (float)Math.Cos(angleMinute) + 180;
             yMin = 100 * (float)Math.Sin(angleMinute) + 130;
-
             xHour = 70 * (float)Math.Cos(angleHour) + 180;
             yHour = 70 * (float)Math.Sin(angleHour) + 130;
 
@@ -90,25 +81,20 @@ namespace Cuckoo_clock
                     label3.Text = label3.Text + "КАР";
                 }
             }
-
             gr.DrawLine(myPenSeconds, 180, 130, xSec, ySec);
             gr.DrawLine(myPenMinute, 180, 130, xMin, yMin);
             gr.DrawLine(myPenMinute, 180, 130, xHour, yHour);
-
             Console.WriteLine(angleSeconds);
             Console.WriteLine(angleMinute);
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
-
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
