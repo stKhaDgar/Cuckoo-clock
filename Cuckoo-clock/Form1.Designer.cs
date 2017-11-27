d7937bd3b6819110843d0092d19ec28e8ac3ba27
stKhaDgar <33667544+stKhaDgar@users.noreply.github.com>
2017-11-27 21:47:42 +0200

    Second arrow ready

--- Cuckoo-clock/Form1.Designer.cs
+++ Cuckoo-clock/Form1.Designer.cs
@@ -29,18 +29,10 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(584, 661);
-            this.label1.TabIndex = 0;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -50,28 +42,37 @@
             this.label2.Size = new System.Drawing.Size(1, 1);
             this.label2.TabIndex = 1;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(-43, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(481, 362);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(584, 661);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.MaximumSize = new System.Drawing.Size(600, 700);
             this.MinimumSize = new System.Drawing.Size(600, 700);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Cuckoo-clock/Form1.cs
+++ Cuckoo-clock/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Cuckoo_clock
 {
     public partial class Form1 : Form
     {
+        private Graphics gr;
+        private Pen myPen;
+        private float x;
+        private float y;
+        int secondNow = DateTime.Now.Second;
+        double angleSeconds;
+        Timer secondNows = new Timer();
+
         public Form1()
         {
             InitializeComponent();
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            gr = pictureBox1.CreateGraphics();
+            angleSeconds = 0;
+            myPen = new Pen(Brushes.Black, 3);
+
+            secondNows.Interval = 1000;
+            secondNows.Tick += new EventHandler(this.secondNowTimer);
+            secondNows.Start();
+            
+        }
+
+        public void secondNowTimer(object sender, EventArgs e)
+        {
+            gr.Clear(Color.White);
+            angleSeconds += 0.2;
+
+            x = 90 * (float)Math.Cos(angleSeconds) + 292;
+            y = 90 * (float)Math.Sin(angleSeconds) + 281;
+
+            gr.DrawLine(myPen, 292, 281, x, y);
+
+            Console.WriteLine("Рисуй сука");
+        }
+
+        private void timeSecondNow(object sender,
+       System.Windows.Forms.PaintEventArgs pe)
+        {
+            Graphics g = pe.Graphics;
+            System.Drawing.Pen myPen;
+            myPen = new System.Drawing.Pen(System.Drawing.Color.Black, 10);
+            
+            int Xposition = (int)(90 * Math.Sin(angleSeconds) + 292);
+            int Yposition = (int)(90 * Math.Cos(angleSeconds) + 281);
+            g.DrawLine(myPen, 292, 281, 400, 400);
+            angleSeconds = angleSeconds + 0.104;
+
+            Console.WriteLine(secondNow);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,6 +69,11 @@ namespace Cuckoo_clock
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
